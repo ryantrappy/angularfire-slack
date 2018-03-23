e9a6540c85cad2742183a5b965494e64a5816084
ryantrappy <ryantrapp@live.com>
2018-03-23 16:22:14 -0500

    initial commit

---
+++ app/auth/auth.controller.js
@@ -0,0 +1,43 @@
+angular.module('angularfireSlackApp')
+  .controller('AuthCtrl', function(Auth, $state){
+    var authCtrl = this;
+
+    authCtrl.user = {
+      email: '',
+      password: ''
+    }
+
+    // authCtrl.login = function (){
+    //   Auth.$signInWithEmailAndPassword(authCtrl.user).then(function (auth){
+    //     $state.go('home');
+    //   }, function (error){
+    //     authCtrl.error = error;
+    //   });
+    // }
+
+    // authCtrl.register = function (){
+    //   Auth.$createUserWithEmailAndPassword(authCtrl.user).then(function (user){
+    //     $state.go('home');
+    //   }, function (error){
+    //     authCtrl.error = error;
+    //   });
+    // }
+
+    authCtrl.login = function (){
+      Auth.$signInWithEmailAndPassword(authCtrl.user.email, authCtrl.user.password).then(function (auth){
+        $state.go('home');
+      }, function (error){
+        authCtrl.error = error;
+      });
+    }
+
+    authCtrl.register = function (){
+      Auth.$createUserWithEmailAndPassword(authCtrl.user.email, authCtrl.user.password).then(function (user){
+        $state.go('home');
+      }, function (error){
+        authCtrl.error = error;
+      });
+    }
+
+
+  });
---
+++ app/channels/channels.controller.js
@@ -0,0 +1,31 @@
+angular.module('angularfireSlackApp')
+  .controller('ChannelsCtrl', function($state, Auth, Users, profile, channels){
+    var channelsCtrl = this;
+    channelsCtrl.profile = profile;
+    channelsCtrl.channels = channels;
+    channelsCtrl.getDisplayName = Users.getDisplayName;
+    channelsCtrl.getGravatar = Users.getGravatar;
+    channelsCtrl.users = Users.all;
+
+    Users.setOnline(profile.$id);
+  
+    channelsCtrl.logout = function(){
+      channelsCtrl.profile.online = null;
+      channelsCtrl.profile.$save().then(function(){
+        Auth.$signOut().then(function(){
+          $state.go('home');
+        });
+      });
+    };
+
+    channelsCtrl.newChannel = {
+      name: ''
+    };
+
+    channelsCtrl.createChannel = function(){
+      channelsCtrl.channels.$add(channelsCtrl.newChannel).then(function(ref){
+        $state.go('channels.messages', {channelId: ref.key});
+      });
+    };
+
+  });
