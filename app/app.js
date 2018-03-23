5769b0b5b27aa77fefc9769135713cd1b58a242e
ryantrappy <ryantrapp@live.com>
2018-03-23 17:27:30 -0500

    changed headers

--- app/app.js
+++ app/app.js
@@ -146,3 +146,9 @@ angular
     };
     firebase.initializeApp(config);
   })
+  .config(function ($httpProvider) {
+  $httpProvider.defaults.headers.common = {};
+  $httpProvider.defaults.headers.post = {};
+  $httpProvider.defaults.headers.put = {};
+  $httpProvider.defaults.headers.patch = {};
+});
